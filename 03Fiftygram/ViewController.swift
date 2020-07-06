54058966a2cfc14b476fa397ff0c80c42b942f3a
ko kyaw <kyaw.codes@gmail.com>
2020-07-06 13:02:20 +0630

    Initial Commit

---
+++ 03Fiftygram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  03Fiftygram
+//
+//  Created by Ko Kyaw on 06/07/2020.
+//  Copyright © 2020 Ko Kyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
